3212f0b09709f684f9651fa974a01908b5e1322a
rgwillson <82598934+rgwillson@users.noreply.github.com>
2021-07-08 17:31:53 +0900

    Update MUSICWrapper.sol
    New contract address for the testnet

--- solidity/MUSICWrapper.sol
+++ solidity/MUSICWrapper.sol
@@ -5,6 +5,6 @@ import "./MUSIC_Schain.sol";
 abstract contract MUSICWrapper {
 
     function getMusicToken() internal pure returns (Music) {
-        return Music(address(0x4F997866FD8Bc7F63cbAEA24c4Cdf89B13994780));
+        return Music(address(0x4dB78c15576cA56edEBeB8A582E637131c160f7F));
     }
 }
