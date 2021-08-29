d9df3c9cf30c116270e700b27e55a9326c267be6
rgwillson <82598934+rgwillson@users.noreply.github.com>
2021-08-29 16:57:18 +0900

    Update hardhat.config.js
    Updated gas definitions due to an issue with Skale's selfdestruct gas estimation logic that under estimates and fails when just using auto

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,22 +1,5 @@
+require("@nomiclabs/hardhat-ethers");
 require("@nomiclabs/hardhat-waffle");
-
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async () => {
-  const accounts = await ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
-
 
 module.exports = {
   defaultNetwork: "skale",
@@ -35,16 +18,15 @@ module.exports = {
     },
     skale: {
       url: "https://dappnet-api.skalenodes.com/v1/elegant-ancha",
-      chainId: 2067916454627834,
       accounts: ["PUT 5 TEST ACCOUNT PRIVATE KEYS HERE.  THEY NEED TO BE HOLDING skETH"],
+      chainId: 0x758c251b409fa,
+      timeout: 50000,
+      gasPrice: "auto",
+      gas: 8000000,
     }
   }
 };
-
-
-
 /**
-
 SKALE TestNet
 S-Chain Name
 elegant-ancha
@@ -59,18 +41,4 @@ https://dappnet-api.skalenodes.com/v1/elegant-ancha
 WebSocket Endpoints
 ws://dappnet-v2-15.skalenodes.com:10066
 wss://dappnet-v2-15.skalenodes.com:10071﻿
-
-IMA Rinkeby ABI
-https://raw.githubusercontent.com/skalenetwork/skale-network/master/releases/dappnet-v2/IMA/1.0.0/abi.json
-
-IMA SChain ABI
-https://github.com/skalenetwork/skale-network/blob/master/releases/dappnet-v2/IMA/1.0.0/proxySchain.json
-
-Supporting IMA Documentation
-
-https://skale.network/docs/developers/products/interchain-messaging-agent/transferring-eth
-https://skale.network/docs/developers/products/interchain-messaging-agent/managing-erc20
-https://skale.network/docs/developers/products/interchain-messaging-agent/managing-erc721
-https://skale.network/docs/developers/products/interchain-messaging-agent/managing-erc1155
-
 */
