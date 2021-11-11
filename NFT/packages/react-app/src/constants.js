08aa10f11daad3f168845182523578ca845a6a2c
bengyles <bengyles@hotmail.com>
2021-11-11 06:29:07 +0100

    Prepare scaffold-eth boilerplate

---
+++ NFT/packages/react-app/src/constants.js
@@ -0,0 +1,101 @@
+// API token for nft.storage. Please sign up and replace this with your own key from https://nft.storage/manage/
+export const NFT_STORAGE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDZmQWJDNjMyNUM2OGI2NTQ1NGI4QUIwOEI3NTIzRjVjRjcwZjg2NkUiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTYzNTMyOTcxODQ1NywibmFtZSI6ImVuZC10by1lbmQtdHV0b3JpYWwifQ.G7bAdCHLaR2r0afNOl7PGoyYwKaLFgiQPCk39MF--K4";
+
+// Infura project ID. If you're building a real app based on this, please replace with your own from https://infura.io/dashboard/ethereum
+export const INFURA_ID = "83e7a8f500f24deeaeaa17b4792ed5bc";
+
+// Key for Etherscan.io. If you're building a real app based on this, please replace with your own from https://etherscan.io/myapikey
+export const ETHERSCAN_KEY = "6EAGC2DEG6PP3M1GP1I86QJPQAUE4ESW1W";
+
+// BLOCKNATIVE ID FOR Notify.js:
+export const BLOCKNATIVE_DAPPID = "0b58206a-f3c0-4701-a62f-73c7243e8c77";
+
+
+export const DEFAULT_CONTRACT_NAME = "NFTMinter";
+
+export const NETWORK = chainId => {
+  for (const n in NETWORKS) {
+    if (NETWORKS[n].chainId === chainId) {
+      return NETWORKS[n];
+    }
+  }
+};
+
+export const NETWORKS = {
+  localhost: {
+    name: "localhost",
+    color: "#666666",
+    chainId: 31337,
+    blockExplorer: "",
+    rpcUrl: "http://" + window.location.hostname + ":8545",
+  },
+  mainnet: {
+    name: "mainnet",
+    color: "#ff8b9e",
+    chainId: 1,
+    rpcUrl: `https://mainnet.infura.io/v3/${INFURA_ID}`,
+    blockExplorer: "https://etherscan.io/",
+  },
+  kovan: {
+    name: "kovan",
+    color: "#7003DD",
+    chainId: 42,
+    rpcUrl: `https://kovan.infura.io/v3/${INFURA_ID}`,
+    blockExplorer: "https://kovan.etherscan.io/",
+    faucet: "https://gitter.im/kovan-testnet/faucet", // https://faucet.kovan.network/
+  },
+  rinkeby: {
+    name: "rinkeby",
+    color: "#e0d068",
+    chainId: 4,
+    rpcUrl: `https://rinkeby.infura.io/v3/${INFURA_ID}`,
+    faucet: "https://faucet.rinkeby.io/",
+    blockExplorer: "https://rinkeby.etherscan.io/",
+  },
+  ropsten: {
+    name: "ropsten",
+    color: "#F60D09",
+    chainId: 3,
+    faucet: "https://faucet.ropsten.be/",
+    blockExplorer: "https://ropsten.etherscan.io/",
+    rpcUrl: `https://ropsten.infura.io/v3/${INFURA_ID}`,
+  },
+  goerli: {
+    name: "goerli",
+    color: "#0975F6",
+    chainId: 5,
+    faucet: "https://goerli-faucet.slock.it/",
+    blockExplorer: "https://goerli.etherscan.io/",
+    rpcUrl: `https://goerli.infura.io/v3/${INFURA_ID}`,
+  },
+  xdai: {
+    name: "xdai",
+    color: "#48a9a6",
+    chainId: 100,
+    price: 1,
+    gasPrice: 1000000000,
+    rpcUrl: "https://dai.poa.network",
+    faucet: "https://xdai-faucet.top/",
+    blockExplorer: "https://blockscout.com/poa/xdai/",
+  },
+  matic: {
+    name: "matic",
+    color: "#2bbdf7",
+    chainId: 137,
+    price: 1,
+    gasPrice: 1000000000,
+    rpcUrl: "https://rpc-mainnet.maticvigil.com",
+    faucet: "https://faucet.matic.network/",
+    blockExplorer: "https://explorer-mainnet.maticvigil.com//",
+  },
+  mumbai: {
+    name: "mumbai",
+    color: "#92D9FA",
+    chainId: 80001,
+    price: 1,
+    gasPrice: 1000000000,
+    rpcUrl: "https://rpc-mumbai.maticvigil.com",
+    faucet: "https://faucet.matic.network/",
+    blockExplorer: "https://mumbai-explorer.matic.today/",
+  },
+};
